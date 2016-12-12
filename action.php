bafab1a06678d6a54e669f97dd2fbe050a1ea16a
student <student>
2016-12-12 16:22:35 +0900

    tmpファイルの削除を実装

--- action.php
+++ action.php
@@ -10,18 +10,20 @@
 	$img = null;
 	
 	// ディレクトリの指定
-	$tmp_dir = __DIR__ . "/tmp";
+	$tmp_dir = __DIR__ . "/tmp/";
 	$img_dir = "./img/menu/";
 	
 	if (isset($_FILES['img'])) {
 		if (move_uploaded_file($_FILES['img']['tmp_name'], $tmp_dir . "/" . basename($_FILES['img']['name']))) {
 			// 名前をユニークに
 			$tmpfile = tempnam($tmp_dir, 'IMG_');
-			$tmp_imgname = pathinfo($tmpfile, PATHINFO_FILENAME) . '.jpg';
+			$tmp_name = pathinfo($tmpfile, PATHINFO_FILENAME);
+			$tmp_imgname = $tmp_name . '.jpg';
 			// 名前を変更後、imgディレクトリへ
 			if (rename($tmp_dir . "/". $_FILES['img']['name'], $img_dir . "/" . $tmp_imgname)) {
 				$img = $tmp_imgname;
 			}
+			unlink($tmp_dir . $tmp_name . ".tmp");
 		}
 	}
 	
